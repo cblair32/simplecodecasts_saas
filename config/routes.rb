da24bc2948c4635b92050dbeb9a13fdfb78d8808
Cameron Blair <cameron.blair2@gmail.com>
2016-12-11 02:59:57 +0000

    Installed Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :contacts
   # This generates a few routes, bundle exec rake routs will show you them
   get "/about" => 'pages#about' 
