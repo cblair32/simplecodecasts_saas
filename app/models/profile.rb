9a231f97476ba470ebe4da5751792c791e3f33a1
Cameron Blair <cameron.blair2@gmail.com>
2016-12-21 19:21:34 +0000

    Added users show action and removed buggy validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,6 @@
 class Profile < ActiveRecord::Base
     belongs_to :user
-    validates :user, presence: true
-    validates :first_name presence: { message:"You must enter a first name"}
-    validates :last_name presence: {messages: "You must enter a last name"}
-    validates :phone_number, numericality: {only_integer: true}
-    validates :description, length: {maximum: 500}
+    
     
     
 end
