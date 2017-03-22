631244a3a4363cc27b3737bec576cb0bfc59a34b
Cameron Blair <cameron.blair2@gmail.com>
2017-03-22 00:39:26 +0000

    New ReadMe

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,24 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
   has_one :profile
-  attr_accessor :stripe_card_token 
+  #has_many :microposts, dependent: :destroy
+  #has_many :active_relationships, class_name: "Relationship",
+  #                                foreign_key: "follower_id",
+  #                                dependent: :destroy
+  #has_many :passive_relationships, class_name: "Relationship",
+  #                                  foreign_key: "follower_id",
+  #                                  dependent: :destroy
+  #has_many :following, through: :active_relationships, source: :followed
+  #has_many :followers, through: :passive_relationships, source: :follower
+  #attr_accessor :stripe_card_token, remember_token, :activation_token, :reset_token
+  #before_save :downcase_email
+  #before_create :create_activation_digest
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  #validates :email, presence: true, length: {maximum: 255},
+  #                  format: {with: VALID_EMAIL_REGEX },
+  #                  uniqueness: {case_sensitive: false}
+  #has_secure_password
+  #validates :password, presence: true, length: {minimum: 6}, allow_nil: true
   
   def save_with_payment
     if valid?
